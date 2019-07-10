ccec10db52a1f669c48dcdee42dc55a17aff5116
༺༽༾ཊ 船长 ཏ༿༼༻ <captaininphw@gmail.com>
2019-07-11 00:22:02 +0800

    Create Bridge.java

---
+++ src/main/java/com/github/blindpirate/extensions/Bridge.java
@@ -0,0 +1,7 @@
+package com.github.blindpirate.extensions;
+
+public class Main {
+    public static Object aCaptureSystemOutputExtension() {
+        return new CaptureSystemOutputExtension();
+    }
+}
