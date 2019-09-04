5091c3db7bf4325b1fdddbc027ecf6ab11109bfa
gaozhidong <1011284295@qq.com>
2019-09-04 12:11:16 +0800

    test

--- src/main/java/com/github/blindpirate/extensions/Gzd.java
+++ src/main/java/com/github/blindpirate/extensions/Gzd.java
@@ -1,7 +1,7 @@
 package com.github.blindpirate.extensions;
 
-public class Bridge {
-    public static Object bridge(){
+public class Gzd {
+    public static Object gzd(){
         return new CaptureSystemOutputExtension();
     }
 }
