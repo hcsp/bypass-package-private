ff4320ca71c7f847dea747ddbf0174204debd585
qiaomengnan <2568151682@qq.com>
2019-10-01 23:26:29 +0800

    commit

---
+++ src/main/java/com/github/blindpirate/extensions/CaptureSystemOutputExtension.java
@@ -0,0 +1,17 @@
+package com.github.blindpirate.extensions;
+
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+/**
+ * @author qiaomengnan
+ * @ClassName: CaptureSystemOutputExtension
+ * @Description:
+ * @date 2019-10-01
+ */
+class CaptureSystemOutputExtension implements BeforeEachCallback
+{
+	@Override public void beforeEach(ExtensionContext extensionContext) throws Exception {
+
+	}
+}
