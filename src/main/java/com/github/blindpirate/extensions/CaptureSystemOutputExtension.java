22d53082f83fb4c338b13d985d832ca4ba1970d8
Lian <L1anyX@126.com>
2019-10-26 16:32:42 +0800

    创建了绕过包级私有的限制的包和方法

---
+++ src/main/java/com/github/blindpirate/extensions/CaptureSystemOutputExtension.java
@@ -0,0 +1,10 @@
+package com.github.blindpirate.extensions;
+
+/**
+ * @Auther:Lian
+ * @Date:2019/10/26 16:30
+ * @Description:TODO
+ * @version:1.0
+ */
+public class CaptureSystemOutputExtension {
+}
