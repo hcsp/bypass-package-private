27eab8bc4043c242c25dab1be9873115b28c6dca
TangsongyuanmingQingHAD <had13625428701@aliyun.com>
2019-12-10 10:02:38 +0800

    通过建立同样的路径的新类绕过private限制的原类

--- src/main/java/com/github/blindpirate/extensions/CaptureSystemOutputExtension.java
+++ src/main/java/com/github/blindpirate/extensions/CaptureSystemOutputExtension.java
@@ -1,4 +1,7 @@
 package com.github.blindpirate.extensions;
 
 public class CaptureSystemOutputExtension {
+    public static CaptureSystemOutputExtension newInstance() {
+        return new CaptureSystemOutputExtension();
+    }
 }
