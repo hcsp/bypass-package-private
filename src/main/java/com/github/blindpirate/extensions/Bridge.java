967efa7c5d393ef9ffceb520a1bcd0c5891ea743
yaohengfeng <1065435454@qq.com>
2019-12-26 13:26:47 +0800

    绕过包级私有

--- src/main/java/com/github/blindpirate/extensions/Bridge.java
+++ src/main/java/com/github/blindpirate/extensions/Bridge.java
@@ -7,7 +7,7 @@ package com.github.blindpirate.extensions;
  */
 public class Bridge {
 
-    public static Object newInstance(){
+    public static Object newInstance() {
         return new CaptureSystemOutputExtension();
     }
 }
