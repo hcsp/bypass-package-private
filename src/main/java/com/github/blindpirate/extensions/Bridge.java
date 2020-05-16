92c515410d8620f631dc270a100e9fdbca6fb6b8
Administrator <756433556@qq.com>
2020-05-17 01:30:50 +0800

    绕过包

--- src/main/java/com/github/blindpirate/extensions/Bridge.java
+++ src/main/java/com/github/blindpirate/extensions/Bridge.java
@@ -1,7 +1,8 @@
 package com.github.blindpirate.extensions;
 
-public class Bridge {
+    public class Bridge {
     public static  Object newInstance() {
+
         return new CaptureSystemOutputExtension();
     }
 }
