fe40929a5b6bd3a13cf7a005e8b85a5066ba49b8
lx <iamxliu@163.com>
2022-04-03 15:43:38 +0800

    11

--- src/main/java/com/github/blindpirate/extensions/Bridge.java
+++ src/main/java/com/github/blindpirate/extensions/Bridge.java
@@ -3,7 +3,5 @@ package com.github.blindpirate.extensions;
 public class Bridge {
     public static Object newInstance() {
         return new CaptureSystemOutputExtension();
-
-
     }
 }
--- src/main/java/com/github/hcsp/encapsulation/Main.java
+++ src/main/java/com/github/hcsp/encapsulation/Main.java
@@ -7,7 +7,6 @@ import com.github.blindpirate.extensions.Bridge;
 
 public class Main {
     public static void main(String[] args) {
-
         System.out.println(createCaptureSystemOutputExtension().getClass().getName());
     }
 
