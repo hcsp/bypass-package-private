0d337ba551578d9f051c5f2d73721a52d5502be2
spearhead0428 <hastur1016@gmail.com>
2023-06-01 23:30:59 +0800

    123123333333

---
+++ src/main/java/com/github/blindpirate/extensions/Brige.java
@@ -0,0 +1,7 @@
+package com.github.blindpirate.extensions;
+
+public class Brige {
+	public  static Object test(){
+		return new CaptureSystemOutputExtension();
+	}
+}
--- src/main/java/com/github/blindpirate/extensions/CaptureSystemOutputExtension.java
+++
@@ -1,6 +0,0 @@
-package com.github.blindpirate.extensions;
-
-public class CaptureSystemOutputExtension{
-	public  static CaptureSystemOutputExtension(){
-	}
-}
