3622b877b1c7d5f44d2a10ca5343a2402ac236db
spearhead0428 <hastur1016@gmail.com>
2023-06-01 23:25:57 +0800

    123123333333

--- src/main/java/com/github/blindpirate/extensions/CaptureSystemOutputExtension.java
+++ src/main/java/com/github/blindpirate/extensions/CaptureSystemOutputExtension.java
@@ -1,7 +1,6 @@
 package com.github.blindpirate.extensions;
 
-public class CaptureSystemOutputExtension {
-	public  static  CaptureSystemOutputExtension CaptureSystemOutputExtension(){
-		return  new CaptureSystemOutputExtension();
+public class CaptureSystemOutputExtension{
+	public  static CaptureSystemOutputExtension(){
 	}
 }
